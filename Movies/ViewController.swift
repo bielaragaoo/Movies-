1fad6e4f9093ecbed22c93b9433d55279085ad02
bielaragaoo <97034610+bielaragaoo@users.noreply.github.com>
2022-04-25 15:55:12 -0300

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Gabriel Aragao on 25/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
